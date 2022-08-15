c99863d0ffa9df688ae93694956fd3b222d8a883
Rushikesh Jadhav <rushikeshdjrrj@gmail.com>
2022-08-16 00:17:01 +0530

    qrcode

---
+++ src/Components/qrcode.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import QRCode from 'react-qr-code'
+
+const qrcode = () => {
+    return (
+        <div>
+            <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+                <QRCode
+                    size={256}
+                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    value="awdawd"
+                    viewBox={`0 0 256 256`}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default qrcode
