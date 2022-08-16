a37b3274acfef0678f030be9c27ac71cf186d497
ajit <ajitdakre05@gmail.com>
2022-08-17 01:01:14 +0530

    Added Responsive in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,16 +5,16 @@ import youtube from "../assets/youtube.svg";
 export default function Footer() {
   return (
     <>
-      <div className="h-80 bg-footer-darkblue flex justify-center mx-10/12 text-white gap-1">
+      <div className="h-full md:h-80 bg-footer-darkblue flex md:flex-row flex-col justify-center items-center text-white gap-5 md:gap-1 py-5 md:py-0">
         {/* div1 */}
-        <div className="w-2/12 text-xl font-bold flex items-center justify-center">
-          10K <br /> user already <br /> connected worldwide
+        <div className="md:w-2/12 text-xl font-bold flex items-center justify-center">
+          10K user already connected worldwide
         </div>
         {/* div2 */}
-        <div className="w-5/12 flex justify-around mx-5 mt-20">
+        <div className="md:w-5/12 flex flex-col md:flex-row justify-around items-center md:items-start mx-5 md:mt-20">
           <div>
             <div className="my-2 font-bold"> Quick Links</div>
-            <div className="flex gap-1 flex-col text-grey-copyright">
+            <div className="flex items-center md:items-start gap-1 flex-col text-grey-copyright">
               <div>Home</div>
               <div>About</div>
               <div>XYZ</div>
@@ -24,20 +24,20 @@ export default function Footer() {
 
           <div>
             <div className="my-2 font-bold"> Get Intouch</div>
-            <div className="flex gap-1 flex-col text-grey-copyright">
+            <div className="flex items-center md:items-start gap-1 flex-col text-grey-copyright">
               <div>Contact us</div>
             </div>
           </div>
 
           <div>
-            <div className="my-2 font-bold">Address</div>
-            <div className="flex gap-1 flex-col text-grey-copyright">
-              <div>Mumbai, Mahrashtra</div>
+            <div className="my-2 font-bold"> Address</div>
+            <div className="text-grey-copyright">
+              <div>Mumbai, MH</div>
             </div>
           </div>
         </div>
         {/* div3 */}
-        <div className="w-3/12 flex items-start justify-center flex-col gap-2">
+        <div className="md:w-3/12 flex items-center justify-center flex-col gap-2 mb-5 md:mb-0">
           <div className="text-bold">Follow Us</div>
           <div className="flex gap-3">
             <img src={instagram} alt="instagram" />
