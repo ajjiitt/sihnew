ea4c5ed32ef035c80db71a5dcc92c7f6e6d5ae42
ajit <ajitdakre05@gmail.com>
2022-08-18 00:52:48 +0530

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import IpfsUpload from "./SubComponents/IpfsUpload";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
-
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <div className="App" style={{ fontFamily: "Inter" }}>
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/ipfs" element={<IpfsUpload />} />
         <Route path="/allcenterdata" element={<AllCenterData />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
