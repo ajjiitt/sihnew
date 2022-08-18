16b20dd25c42bb240a857572858c5309a77801a7
ajit <ajitdakre05@gmail.com>
2022-08-19 02:50:02 +0530

    Fixed Routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import NotFound from "./pages/NotFound";
+import Admin from "./pages/Admin";
+import ViewDisaster from "./pages/ViewDisaster";
 function App() {
   return (
     <div className="App" style={{ fontFamily: "Inter" }}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route exact path="/admin" element={<Admin />}/>
         <Route path="/ipfs" element={<IpfsUpload />} />
         <Route path="/allcenterdata" element={<AllCenterData />} />
-        <Route exact path="/admin" element={<Admin />}/>
+        <Route path="/disaster" element={<ViewDisaster />}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
