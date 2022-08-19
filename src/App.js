3adb860d44ffb656093984c76cb980353cb6c578
ajit <ajitdakre05@gmail.com>
2022-08-20 01:31:19 +0530

    Added ViewState

--- src/App.js
+++ src/App.js
@@ -10,21 +10,25 @@ import ViewDisaster from "./pages/ViewDisaster";
 import DemandForm from "./pages/DemandForm";
 import SupplyForm from "./pages/SupplyForm";
 import RegisterForm from "./pages/RegisterForm";
+import ViewState from "./pages/ViewState";
 function App() {
   return (
-    <div className="App flex flex-col justify-between h-screen" style={{ fontFamily: "Inter" }}>
+    <div
+      className="App flex flex-col justify-between h-screen"
+      style={{ fontFamily: "Inter" }}
+    >
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route exact path="/admin" element={<Admin />}/>
+        <Route exact path="/admin" element={<Admin />} />
         <Route path="/ipfs" element={<IpfsUpload />} />
         <Route path="/allcenterdata" element={<AllCenterData />} />
-        <Route path="/disaster" element={<ViewDisaster />}/>
-        <Route path="/demand" element={<DemandForm />}/>
-        <Route path="/supply" element={<SupplyForm />}/>
-        <Route path="/registerfrom" element={<RegisterForm />}/>
+        <Route path="/disaster" element={<ViewDisaster />} />
+        <Route path="/demand" element={<DemandForm />} />
+        <Route path="/supply" element={<SupplyForm />} />
+        <Route path="/registerform" element={<RegisterForm />} />
+        <Route path="/state" element={<ViewState />} />
         <Route path="*" element={<NotFound />} />
-
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/ViewState.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ViewState() {
+  return (
+    <div>ViewState</div>
+  )
+}
