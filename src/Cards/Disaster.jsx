de0fa57ea2fb441a231c67832c9e76da4087b15b
ajit <ajitdakre05@gmail.com>
2022-08-21 04:51:50 +0530

    Updated UI

--- src/Cards/Disaster.jsx
+++ src/Cards/Disaster.jsx
@@ -1,42 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Disaster = ({name, location}) => {
+const Disaster = ({ name, location, description }) => {
   return (
-    <div class="p-5 md:w-1/3 m-2 rounded-lg border border-gray-700 shadow-md flex-col">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">
-                {name}
-              </h5>
+    <div class="p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md max-h-52">
+      <p class="mb-2 w-full h-10 text-gray-500 dark:text-gray-600 text-lg">
+        Location: {location}
+      </p>
+      <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-footer-darkblue">
+        {name}
+      </h5>
+      <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 break-all truncate">
+        {description ? description : "No description"}
+      </p>
+      <a
+        href="#"
+        class="inline-flex items-center text-blue-600 hover:underline"
+      >
+        View Disaster
+        <svg
+          class="ml-2 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+        </svg>
+      </a>
+    </div>
+  );
+};
 
-              <p class="mb-3 font-normal text-gray-900 text-center">
-                Location
-              </p>
-              <p class="mb-3 font-normal text-gray-500 text-center">
-              {location}
-              </p>
-              <div className='flex justify-center items-center'>
-
-              <button
-                
-                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                View
-                <svg
-                  aria-hidden="true"
-                  class="ml-2 -mr-1 w-4 h-4"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                    ></path>
-                </svg>
-              </button>
-                </div>
-            </div>
-  )
-}
-
-export default Disaster
+export default Disaster;
--- src/Components/ViewDisasters.jsx
+++ src/Components/ViewDisasters.jsx
@@ -3,36 +3,38 @@ import Disaster from "../Cards/Disaster";
 import { intializeMasterContract } from "../Utils/connectWallet";
 
 const ViewDisasters = () => {
-    const [disasters, setDisasters] = useState([]);
+  const [disasters, setDisasters] = useState([]);
 
-    const fetchDisasters = async () => {
-        const contract = intializeMasterContract();
-        const allDisasters = await contract.methods.getDisasters().call();
-        console.log(allDisasters);
-        setDisasters(allDisasters);
-    }
+  const fetchDisasters = async () => {
+    const contract = intializeMasterContract();
+    const allDisasters = await contract.methods.getDisasters().call();
+    console.log(allDisasters);
+    setDisasters(allDisasters);
+  };
 
-    useEffect(() => {
-        fetchDisasters();
-    }, [])
+  useEffect(() => {
+    fetchDisasters();
+  }, []);
 
   return (
     <div>
-      <div class="p-6 my-5 mx-5 bg-white rounded-lg border border-gray-200 shadow-md  ">
-        <h5 class="mb-2 text-5xl font-semibold tracking-tight text-center text-gray-900 pt-4 pb-6">
+      <div class="p-6 md:my-5">
+        <h5 class="mb-2 text-3xl md:text-5xl font-semibold tracking-tight text-center text-gray-900 md:pt-4 pb-6">
           DISASTERS
         </h5>
 
-        <div class="bg-white rounded-lg border border-gray-200 shadow-md  dark:border-gray-700">
-          <div className="w-full md:flex flex-wrap justify-center items-start">
-            {
-                disasters.map((ele, index) => {
-                    return (
-                        <Disaster name={ele.disasterName} location={ele.location} contract={ele.disasterContract} key={index}/>
-                    );
-                })
-            }
-      
+        <div class="">
+          <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+            {disasters.map((ele, index) => {
+              return (
+                <Disaster
+                  name={ele.disasterName}
+                  location={ele.location}
+                  contract={ele.disasterContract}
+                  key={index}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
