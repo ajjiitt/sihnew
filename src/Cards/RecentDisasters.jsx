813c91440171b9338709bd2e1b1135168673b2fe
ajit <ajitdakre05@gmail.com>
2022-08-25 13:02:18 +0530

    UPdated FrontENd ui

--- src/Cards/RecentDisasters.jsx
+++ src/Cards/RecentDisasters.jsx
@@ -1,95 +1,69 @@
-import React from 'react'
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import React, { useState, useEffect } from "react";
+import { db } from "../Utils/firebaseConfig";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import {
+  collection,
+  getDocs,
+  limit,
+  query,
+  addDoc,
+  doc,
+} from "firebase/firestore";
 const RecentDisasters = () => {
-    return (
-        <div className=" px-3  rounded-lg   ">
-            <div className='flex flex-row items-center justify-start pb-3'>
-                <div className='w-11 h-0' style={{ borderTop: "3px solid #c5b58f" }}>
-                </div>
-                <div className='pl-1 tracking-wider text-xl font-semibold  text-buttonOrange' >
-                    Recent Disasters
-                </div>
-            </div>
-            <div className="flex flex-col pl-2 pt-4 gap-2 max-h-[31rem] scrollbar-thin scrollbar-thumb-orange-400 scrollbar-track-orange-100 overflow-y-scroll">
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-                <div className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer ">
-                    <KeyboardDoubleArrowRightIcon className="" style={{ height: "25px", width: "25px" }} />
-                    <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>cyclone in uttarpradesh ,24 people lost,13 people missing</p>
-                </div>
-                <hr />
-
-            </div>
-        </div>
+  const [newsDisaster, setNewsDisaster] = useState([]);
+  const getData = async () => {
+    const querySapshoot = await getDocs(
+      query(collection(db, "disasters"), limit(10))
     )
-}
+      .then((res) => {
+        let tempARr = [];
+        res.forEach((doc) => {
+          console.log(doc.data());
+          tempARr.push(doc.data());
+        });
+        console.log(tempARr);
+        setNewsDisaster(tempARr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(newsDisaster);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className=" px-3  rounded-lg   ">
+      <div className="flex flex-row items-center justify-start pb-3">
+        <div
+          className="w-11 h-0"
+          style={{ borderTop: "3px solid #c5b58f" }}
+        ></div>
+        <div className="pl-1 tracking-wider text-xl font-semibold  text-buttonOrange">
+          Recent Disasters
+        </div>
+      </div>
+      <div className="flex flex-col pl-2 pt-4 gap-2 max-h-[31rem] scrollbar-thin scrollbar-thumb-orange-400 scrollbar-track-orange-100 overflow-y-scroll">
+        {newsDisaster?.map((d) => (
+          <>
+            <div
+              onClick={() => window.open(d.link, "_blank")}
+              className="flex flex-row items-center hover:text-buttonOrange hover:pl-1 justify-center cursor-pointer "
+            >
+              <KeyboardDoubleArrowRightIcon
+                className=""
+                style={{ height: "25px", width: "25px" }}
+              />
+              <p className=" ml-2 pr-4" style={{ fontSize: "13px" }}>
+                {d.description}
+              </p>
+            </div>
+            <hr />
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default RecentDisasters
+export default RecentDisasters;
