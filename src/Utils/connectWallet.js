c6a5dc1795950a96af9125cbf26bab28de021794
Tejas <tejaswarambhe@gmail.com>
2022-08-26 13:08:15 +0530

    contract change

--- src/Utils/connectWallet.js
+++ src/Utils/connectWallet.js
@@ -3,8 +3,8 @@ import MasterContractABI from './MasterContractABI.json';
 import DisasterContractABI from './DisasterContractABI.json';
 // import RegisterDataABI from './RegisterDataABI.json';
 
-// const MasterContractAddress = "0xdd28763CEf20664047Bb08a1eB457d56b0FCef49";
-const MasterContractAddress = "0x73eF28D5d4A0519c007C42d0Ba34Fe4EF96362fD";
+const MasterContractAddress = "0x70EDaEEb7767f5e47f325e42a98FfE9159e91285";
+// const MasterContractAddress = "0x73eF28D5d4A0519c007C42d0Ba34Fe4EF96362fD";
 // const RegisterDataContractAddress = "0xcDb089c1B785Db76C607Ce4C4C8C1eD6EB4c50B8";
 // const DisasterContractAddress = "0xb8EC8BA2Be1781C3Ac6B9cF7B5204FFe9f34885C";
 
